e08ebab363fb37e731aa337801ef7d7daa5d8d24
LXTater <Tater@LXTater.com>
2026-06-18 05:13:49 -0500

    Delete TaterStabberPro/V0.1 directory

--- TaterStabberPro/V0.1/newdebug.lua
+++
@@ -1,6 +0,0 @@
-return function ()
-    -- Dumps information about the current programmer part object.
-    Printf("=============== START OF DUMP ===============")
-    DataPool().Universes[1]:Dump()
-    Printf("================ END OF DUMP ================")
-end
